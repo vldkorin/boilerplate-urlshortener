2e32c36da77102d899a4b659a05e018bba76df6a
Vladyslav Korin <korinvlad2@gmail.com>
2025-04-11 18:15:41 +0000

    fix

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const dns = require("dns");
 const mongoose = require('mongoose');
-// Basic Configuration
+
+
 const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -13,13 +14,15 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+
 const urlSchema = new mongoose.Schema({
   originalUrl: String,
-  shortUrl: String
+  shortUrl: Number
 });
 
 const Url = mongoose.model('Url', urlSchema);
@@ -27,47 +30,53 @@ const Url = mongoose.model('Url', urlSchema);
 
 app.post('/api/shorturl', (req, res, next) => {
   const inputUrl = req.body.url;
-  const urlObj = new URL(inputUrl);
-  let host = urlObj.host;
-  const shortUrl = Math.floor(Math.random() * 99999).toString(); 
 
-  dns.lookup(host, (err, address) => {
-    if (err) {
-      return res.json({error: "Invalid URL"});
-    }
+  try {
+    const urlObj = new URL(inputUrl); 
+    let host = urlObj.host;
 
-    const url = new Url({
-      originalUrl: inputUrl,
-      shortUrl: shortUrl
-    });
+    const shortUrl = Math.floor(Math.random() * 100000); 
 
-    url.save((err, data) => {
+    dns.lookup(host, (err, address) => {
       if (err) {
-        return res.json({error: "Database err"});
+        return res.json({error: "Invalid URL"});
       }
-
-      res.json({
+      const url = new Url({
         originalUrl: inputUrl,
         shortUrl: shortUrl
       });
+
+      url.save((err, data) => {
+        if (err) {
+          return res.json({error: "Database error"});
+        }
+
+        res.json({
+          original_url: inputUrl,
+          short_url: shortUrl
+        });
+      });
     });
-  });
+  } catch (e) {
+    return res.json({error: "Invalid URL"}); 
+  }
 });
 
 app.get("/api/shorturl/:shortUrl", (req, res) => {
-  const shortUrl = req.params.shortUrl;
+  const shortUrl = parseInt(req.params.shortUrl); 
 
   Url.findOne({ shortUrl: shortUrl }, (err, data) => {
     if (err) {
       return res.json({error: "Database error"}); 
     }
+
     if (data) {
-      res.redirect(data.originalUrl)
+      res.redirect(data.originalUrl);
     } else {
       res.json({error: "No short URL found"});
     }
   });
-})
+});
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
